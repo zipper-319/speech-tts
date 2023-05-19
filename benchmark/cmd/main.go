47343dbf46872b77f5ed9bc7806c102fbe3b4d50
zipper <zipper.zhao@cloudminds.com>
2023-05-19 11:41:52 +0800

    fix :  添加客户端参数配置

--- benchmark/cmd/main.go
+++ benchmark/cmd/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"speech-tts/benchmark"
 	"sync"
 )
 
+var threadNum int
+var useCaseNum int
+
+func init()  {
+	flag.IntVar(&threadNum, "t", 1, "threadNum number, eg: -t 1")
+	flag.IntVar(&useCaseNum, "u", 10, "useCaseNum number, eg: -u 10")
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+	flag.Parse()
 	addr := "127.0.0.1:9000"
 	text := "成都今天的天气"
 	speaker := "DaXiaoFang"
 	wg := sync.WaitGroup{}
-	for i := 0; i <= 5; i++ {
+	for i := 0; i <= threadNum; i++ {
 		wg.Add(1)
 		go func() {
-			for j := 0; j <= 10000; j++ {
+			for j := 0; j <= useCaseNum; j++ {
 
 				if err := benchmark.TestTTSV1(addr, text, speaker); err != nil {
 					log.Println("_________")

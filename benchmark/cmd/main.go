671e0cc3ef4a32f1672f97c2da5cf575df74dd2a
zipper <zipper.zhao@cloudminds.com>
2023-05-19 13:08:43 +0800

    fix :  添加客户端参数配置

--- benchmark/cmd/main.go
+++ benchmark/cmd/main.go
@@ -10,22 +10,23 @@ import (
 var threadNum int
 var useCaseNum int
 
-func init()  {
-	flag.IntVar(&threadNum, "t", 1, "threadNum number, eg: -t 1")
-	flag.IntVar(&useCaseNum, "u", 10, "useCaseNum number, eg: -u 10")
+func init() {
+	flag.IntVar(&threadNum, "t", 1, "thread number, eg: -t 1")
+	flag.IntVar(&useCaseNum, "u", 10, "useCase number, eg: -u 10")
 }
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
 	flag.Parse()
+	log.Printf("thread number:%d; useCase number:%d", threadNum, useCaseNum)
 	addr := "127.0.0.1:9000"
 	text := "成都今天的天气"
 	speaker := "DaXiaoFang"
 	wg := sync.WaitGroup{}
-	for i := 0; i <= threadNum; i++ {
+	for i := 0; i < threadNum; i++ {
 		wg.Add(1)
 		go func() {
-			for j := 0; j <= useCaseNum; j++ {
+			for j := 0; j < useCaseNum; j++ {
 
 				if err := benchmark.TestTTSV1(addr, text, speaker); err != nil {
 					log.Println("_________")

b6501a977dd4852aa69033dfa5757623e535e787
zipper <zipper.zhao@cloudminds.com>
2023-05-19 15:05:56 +0800

    fix :  添加客户端参数配置

--- internal/server/server.go
+++ internal/server/server.go
@@ -14,6 +14,7 @@ import (
 	"google.golang.org/grpc/status"
 	"reflect"
 	"runtime/debug"
+	v1 "speech-tts/api/tts/v1"
 	"speech-tts/internal/pkg/trace"
 	"strings"
 	"time"
@@ -110,11 +111,15 @@ type validator interface {
 
 func (w *wrappedStream) RecvMsg(m interface{}) error {
 	log.NewHelper(w.Logger).Infof("Receive a message (Type: %T) after %dms", m, time.Since(w.firstTime).Milliseconds())
-	if v, ok := m.(validator); ok {
-		if err := v.Validate(); err != nil {
-			return status.Errorf(codes.InvalidArgument, "Panic err: %v",err)
-		}
+	if req, ok := m.(*v1.TtsReq); ok {
+		//if v, ok := m.(validator); ok {
+		//	if err := v.Validate(); err != nil {
+		//		return status.Errorf(codes.InvalidArgument, "Panic err: %v", err)
+		//	}
+		//}
+		log.NewHelper(w.Logger).Infof("Receive tts call req: %v", req)
 	}
+
 	return w.ServerStream.RecvMsg(m)
 }
 

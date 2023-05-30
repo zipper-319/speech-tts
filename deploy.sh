fcd2a8a0afd192b4a1c34dda3b913ed7e05c5918
zipper <zipper.zhao@cloudminds.com>
2023-05-30 10:18:37 +0800

    fix: 修改编译启动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,26 @@ user="tts"
 password="Q9dMZp_pGX"
 CI_COMMIT_TAG=`git log --pretty=format:"%h" -1`
 DOCKER_REGISTRY_HOST="harbor.cloudminds.com"
+VERSION="v4.1.6"
+
+expect <<EOF
+set timeout 30
+spawn ssh 172.16.31.72 -p 10022 -l root
+expect {
+"*(yes/no)?" { send "yes\r",exp_continue }
+"*password"  { send "123456\r" }
+}
+expect ":~#" { send "cd speech-tts\r" }
+expect ":~/speech-tts#" { send "bash build.sh $VERSION $AppName\r" }
+expect ":~/speech-tts#" { send "exit\r" }
+EOF
+
 expect -c '
-  set project [ lindex $argv 0 ]
   spawn scp -P  10022 root@172.16.31.72:~/speech-tts/bin/$project ./bin
   expect "*password"
   send "123456\r"
   interact
 '
-VERSION="v4.1.6"
 
 docker build  --no-cache -t harbor.cloudminds.com/$AppName/$IMAGE_NAME:$VERSION.$CI_COMMIT_TAG .
 echo DOCKER_REGISTRY_USER=$user DOCKER_REGISTRY_PASSWORD=$password

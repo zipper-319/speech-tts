391e5d38a3b6b90deab3a09220831be4247df5fe
zipper <zipper.zhao@cloudminds.com>
2023-12-28 17:49:41 +0800

    fix: 添加md信息

--- internal/server/server.go
+++ internal/server/server.go
@@ -262,6 +262,8 @@ func streamInterceptor(logger log.Logger) grpc.StreamServerInterceptor {
 			log.NewHelper(logger).Errorf("------------RPC failed with error: %v", err)
 			return status.Errorf(code, err.Error())
 		}
+		md := metadata.Pairs("cost",fmt.Sprintf("%d", time.Since(now).Milliseconds()))
+		ss.SendHeader(md)
 		return err
 	}
 }

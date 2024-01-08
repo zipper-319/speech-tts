35f29c6005467a2c0386f47fc2d4c6b3d4f9be9d
zipper <zipper.zhao@cloudminds.com>
2024-01-08 17:56:02 +0800

    fix: 修改modelx.sh

--- modelx.sh
+++ modelx.sh
@@ -22,7 +22,15 @@ mkdir -p $MODEL_PATH/res/animation
 cp -r $MODULE_DIR/../animation/res/* $MODEL_PATH/res/animation
 cd $MODEL_Dir/$MODEL_PATH
 
-/usr/bin/expect ./login.exp
+/usr/bin/expect -c '
+  set timeout 30
+  set TOKEN "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ6aXBwZXIuemhhbyIsImV4cCI6MTA4OTM3NTMwNzh9.Ji1YzUKOFTBSxRSa_BhPspRWD5A9mxFvbz1_CPuDTKK81QdU0xveEGp8GgUYJ6JMHFvQXFNjLo-kaQCgfICbiJjgoU67hC5OYf_5r9Au-4--XWabkWYbBiB10HnjBmmQP8_GbDgoa3sp3S0tKuIs-o4WGB8rYGx4M7_85TGYNcMlF6NwM8wg4UyeiQL-zbQnNqcdy7k8Kl3K-yN_95ZB8VAtSNnwIdXm6b1HotRkjVC2NO29AMgqdSIoCjxQNaad9mXGJkpRYCxmL90OV3x-HOSmVUkrlKYf5W5I_yd7wAwAtUpDIPcP5O7ay4plzNoH70t_9TFmN_6ihZPQ4O3w8w"
+  puts $TOKEN
+  spawn modelx login modelx
+  expect "Token:"
+  send "$TOKEN/r"
+  interact
+'
 
 modelx list modelx/speech-tts/ttsv2-model
 modelx push  modelx/speech-tts/ttsv2-model@${VERSION}

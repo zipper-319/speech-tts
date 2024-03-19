60ccd303771a9f595ed82b9e14f8feb89abddef8
zipper <zipper.zhao@cloudminds.com>
2024-03-19 13:54:02 +0800

    fix: 修改modelx脚本

--- local_modelx.sh
+++ local_modelx.sh
@@ -30,15 +30,7 @@ cp $sourceDir/libCmTts.so.online_voicetuning $MODEL_PATH/libCmTts.so.online_voic
 cp -r $sourceDir/res/* $MODEL_PATH/res
 cd $MODEL_Dir/$MODEL_PATH
 
-expect -c '
-        set timeout 30
-        TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ6aXBwZXIuemhhbyIsImV4cCI6MTA4OTM3NTMwNzh9.Ji1YzUKOFTBSxRSa_BhPspRWD5A9mxFvbz1_CPuDTKK81QdU0xveEGp8GgUYJ6JMHFvQXFNjLo-kaQCgfICbiJjgoU67hC5OYf_5r9Au-4--XWabkWYbBiB10HnjBmmQP8_GbDgoa3sp3S0tKuIs-o4WGB8rYGx4M7_85TGYNcMlF6NwM8wg4UyeiQL-zbQnNqcdy7k8Kl3K-yN_95ZB8VAtSNnwIdXm6b1HotRkjVC2NO29AMgqdSIoCjxQNaad9mXGJkpRYCxmL90OV3x-HOSmVUkrlKYf5W5I_yd7wAwAtUpDIPcP5O7ay4plzNoH70t_9TFmN_6ihZPQ4O3w8w"
-        spawn modelx login modelx
-        expect "Token:"
-        send "$TOKEN\r"
-        interact
-
-    '
+modelx login modelx --token eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ6aXBwZXIuemhhbyIsImV4cCI6MTA4OTM3NTMwNzh9.Ji1YzUKOFTBSxRSa_BhPspRWD5A9mxFvbz1_CPuDTKK81QdU0xveEGp8GgUYJ6JMHFvQXFNjLo-kaQCgfICbiJjgoU67hC5OYf_5r9Au-4--XWabkWYbBiB10HnjBmmQP8_GbDgoa3sp3S0tKuIs-o4WGB8rYGx4M7_85TGYNcMlF6NwM8wg4UyeiQL-zbQnNqcdy7k8Kl3K-yN_95ZB8VAtSNnwIdXm6b1HotRkjVC2NO29AMgqdSIoCjxQNaad9mXGJkpRYCxmL90OV3x-HOSmVUkrlKYf5W5I_yd7wAwAtUpDIPcP5O7ay4plzNoH70t_9TFmN_6ihZPQ4O3w8w
 
 modelx list modelx/speech-tts/ttsv2-model
 modelx push  modelx/speech-tts/ttsv2-model@$VERSION
